5d61ec3e7f2c55f2f49fb5fd8ed9b577b46ceb6e
Thomas Fini Hansen <xen@xen.dk>
2019-10-23 23:22:47 +0200

    Validate checkpoint parameters
    To avoid ugly exceptions.

--- spec/CheckpointSpec.php
+++ spec/CheckpointSpec.php
@@ -5,14 +5,14 @@ namespace spec\Rorschach;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Rorschach\Checkpoint;
-use RuntimeException;
+use Rorschach\Exceptions\RorschachError;
 
 class CheckpointSpec extends ObjectBehavior
 {
     function it_should_require_a_path()
     {
         $this->beConstructedWith('test', []);
-        $this->shouldThrow(new RuntimeException(sprintf(Checkpoint::MISSING_PATH_ERROR, 'test')))->duringInstantiation();
+        $this->shouldThrow(new RorschachError(sprintf(Checkpoint::MISSING_PATH_ERROR, 'test')))->duringInstantiation();
     }
 
     function it_allows_string_as_path_shorthand()
@@ -31,17 +31,132 @@ class CheckpointSpec extends ObjectBehavior
 
     function it_should_take_defaults()
     {
-        $this->beConstructedWith('test', 'path', ['hide' => 'tohide']);
+        $this->beConstructedWith('test', 'path', ['hide' => ['name' => 'tohide']]);
         $this->name->shouldBe('test');
         $this->path->shouldBe('/path');
-        $this->hide->shouldBe('tohide');
+        $this->hide->shouldBe(['name' => 'tohide']);
     }
 
     function it_should_let_values_override_defaults()
     {
-        $this->beConstructedWith('test', ['path' => 'path', 'hide' => 'hidden'], ['hide' => 'default']);
+        $this->beConstructedWith('test', ['path' => 'path', 'hide' => ['name' => 'hidden']], ['hide' => ['another name' => 'default']]);
         $this->name->shouldBe('test');
         $this->path->shouldBe('/path');
-        $this->hide->shouldBe('hidden');
+        $this->hide->shouldBe(['name' => 'hidden']);
+    }
+
+    function it_should_validate_hide()
+    {
+        $this->beConstructedWith('test', ['path' => 'path']);
+        $this->validateHide(['key' => 'value'])
+            ->shouldReturn(true);
+        $this->validateHide(['key' => 'value', 'key2' => 'value2'])
+            ->shouldReturn(true);
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_HIDE_ERROR))
+            ->duringValidateHide('banana');
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_HIDE_ERROR))
+            ->duringValidateHide(['banana' => []]);
+    }
+
+    function it_should_validate_browser_width_and_height()
+    {
+        $this->beConstructedWith('test', ['path' => 'path']);
+        $this->validateBrowserHeight('1')
+            ->shouldReturn(true);
+        $this->validateBrowserHeight('500')
+            ->shouldReturn(true);
+        $this->validateBrowserHeight('9999')
+            ->shouldReturn(true);
+
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_BROWSER_SIZE_ERROR))
+            ->duringValidateBrowserHeight('2 bannaas');
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_BROWSER_SIZE_ERROR))
+            ->duringValidateBrowserHeight("0");
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_BROWSER_SIZE_ERROR))
+            ->duringValidateBrowserHeight("-6");
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_BROWSER_SIZE_ERROR))
+            ->duringValidateBrowserHeight("10000");
+
+        $this->validateBrowserWidth('1')
+            ->shouldReturn(true);
+        $this->validateBrowserWidth('500')
+            ->shouldReturn(true);
+        $this->validateBrowserWidth('9999')
+            ->shouldReturn(true);
+
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_BROWSER_SIZE_ERROR))
+            ->duringValidateBrowserWidth('2 bannaas');
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_BROWSER_SIZE_ERROR))
+            ->duringValidateBrowserWidth("0");
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_BROWSER_SIZE_ERROR))
+            ->duringValidateBrowserWidth("-6");
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_BROWSER_SIZE_ERROR))
+            ->duringValidateBrowserWidth("10000");
+    }
+
+    function it_should_validate_wait()
+    {
+        $this->beConstructedWith('test', ['path' => 'path']);
+        $this->validateWait('0')
+            ->shouldReturn(true);
+        $this->validateWait('500')
+            ->shouldReturn(true);
+        $this->validateWait('7200')
+            ->shouldReturn(true);
+
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_WAIT_ERROR))
+            ->duringValidateWait('2 bannaas');
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_WAIT_ERROR))
+            ->duringValidateWait("-6");
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_WAIT_ERROR))
+            ->duringValidateWait("10000");
+    }
+
+    function it_should_validate_stich_delay()
+    {
+        $this->beConstructedWith('test', ['path' => 'path']);
+        $this->validateStitchDelay('0')
+            ->shouldReturn(true);
+        $this->validateStitchDelay('500')
+            ->shouldReturn(true);
+        $this->validateStitchDelay('7200')
+            ->shouldReturn(true);
+
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_STITCH_DELAY_ERROR))
+            ->duringValidateStitchDelay('2 bannaas');
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_STITCH_DELAY_ERROR))
+            ->duringValidateStitchDelay("-6");
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_STITCH_DELAY_ERROR))
+            ->duringValidateStitchDelay("10000");
+    }
+
+    function it_should_validate_wait_script()
+    {
+        $this->beConstructedWith('test', ['path' => 'path']);
+        $this->validateWaitScript('')
+            ->shouldReturn(true);
+        $this->validateWaitScript('banana')
+            ->shouldReturn(true);
+
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_WAIT_SCRIPT_ERROR))
+            ->duringValidateWaitScript(['2 bannaas']);
+    }
+
+    function it_should_validate_dont_kill_animations()
+    {
+        $this->beConstructedWith('test', ['path' => 'path']);
+        $this->validateDontKillAnimations(true)
+            ->shouldReturn(true);
+        $this->validateDontKillAnimations(false)
+            ->shouldReturn(true);
+
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_DONT_KILL_ANIMATIONS_ERROR))
+            ->duringValidateDontKillAnimations('true');
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_DONT_KILL_ANIMATIONS_ERROR))
+            ->duringValidateDontKillAnimations('1');
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_DONT_KILL_ANIMATIONS_ERROR))
+            ->duringValidateDontKillAnimations(1);
+        $this->shouldThrow(new RorschachError(Checkpoint::VALIDATE_DONT_KILL_ANIMATIONS_ERROR))
+            ->duringValidateDontKillAnimations([]);
     }
 }
--- src/Checkpoint.php
+++ src/Checkpoint.php
@@ -2,11 +2,18 @@
 
 namespace Rorschach;
 
-use RuntimeException;
+use Rorschach\Exceptions\RorschachError;
 
 class Checkpoint
 {
     public const MISSING_PATH_ERROR = 'No path for checkpoint "%s".';
+    public const VALIDATE_HIDE_ERROR = '"hide" should be a mapping of <name>: <CSS selector>';
+    public const VALIDATE_BROWSER_SIZE_ERROR = '"browser_height" and "browser_width" should be ' .
+        'a number between 1 and 9999, inclusive.';
+    public const VALIDATE_WAIT_ERROR = '"wait" should be a number between 0 and 7200, inclusive.';
+    public const VALIDATE_STITCH_DELAY_ERROR = '"stitch_delay" should be a number between 0 and 7200, inclusive.';
+    public const VALIDATE_WAIT_SCRIPT_ERROR = '"wait_script" should be a string.';
+    public const VALIDATE_DONT_KILL_ANIMATIONS_ERROR = '"dont_kill_animations" should be true/false.';
 
     public $name;
     public $path;
@@ -26,7 +33,7 @@ class Checkpoint
         }
 
         if (!isset($checkpoint['path'])) {
-            throw new RuntimeException(sprintf(self::MISSING_PATH_ERROR, $name));
+            throw new RorschachError(sprintf(self::MISSING_PATH_ERROR, $name));
         }
         // Ensure all paths starts with a slash.
         $this->path = '/' . ltrim($checkpoint['path'], '/');
@@ -46,8 +53,82 @@ class Checkpoint
             $prop = lcfirst(str_replace('_', '', ucwords($key, '_')));
 
             if (isset($checkpoint[$key])) {
+                if (\method_exists($this, 'validate' . $prop)) {
+                    $this->{'validate' . $prop}($checkpoint[$key]);
+                }
+
                 $this->{$prop} = $checkpoint[$key];
             }
         }
+    }
+
+    public function validateHide($val)
+    {
+        if (is_array($val)) {
+            foreach ($val as $key => $val) {
+                if (!\is_string($key) || !\is_string($val)) {
+                    throw new RorschachError(self::VALIDATE_HIDE_ERROR);
+                }
+            }
+
+            return true;
+        }
+
+        throw new RorschachError(self::VALIDATE_HIDE_ERROR);
+    }
+
+    public function validateBrowserHeight($val)
+    {
+        if ($this->validateInt($val, 1, 9999)) {
+            return true;
+        }
+        throw new RorschachError(self::VALIDATE_BROWSER_SIZE_ERROR);
+    }
+
+    public function validateBrowserWidth($val)
+    {
+        return $this->validateBrowserHeight($val);
+    }
+
+    public function validateWait($val)
+    {
+        if ($this->validateInt($val, 0, 7200)) {
+            return true;
+        }
+        throw new RorschachError(self::VALIDATE_WAIT_ERROR);
+    }
+
+    public function validateStitchDelay($val)
+    {
+        if ($this->validateInt($val, 0, 7200)) {
+            return true;
+        }
+        throw new RorschachError(self::VALIDATE_STITCH_DELAY_ERROR);
+    }
+
+    public function validateWaitScript($val)
+    {
+        if (is_string($val)) {
+            return true;
+        }
+        throw new RorschachError(self::VALIDATE_WAIT_SCRIPT_ERROR);
+    }
+
+    public function validateDontKillAnimations($val)
+    {
+        if (is_bool($val)) {
+            return true;
+        }
+        throw new RorschachError(self::VALIDATE_DONT_KILL_ANIMATIONS_ERROR);
+    }
+
+    protected function validateInt($val, $min, $max)
+    {
+        // is_numeric allows for floats and scientific notation, so we'll just
+        // use a regexp.
+        if (preg_match('{^\d+$}', $val) && intval($val) >= $min && \intval($val) <= $max) {
+            return true;
+        }
+        return false;
     }
 }
